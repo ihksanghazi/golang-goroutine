466b72a4f28d5ecf985412a0e0ccc1985d325bbc
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-06-21 03:40:32 +0700

    defeault select channel

--- channel_test.go
+++ channel_test.go
@@ -120,3 +120,29 @@ func TestRangeChannel(t *testing.T) {
 			}
 		}
 	}
+	func TestDefaultSelectChannel(t *testing.T) {
+		channel1 := make(chan string)
+		channel2 := make(chan string)
+		defer close(channel1)
+		defer close(channel2)
+
+		go giveMeResponse(channel1)
+		go giveMeResponse(channel2)
+
+		counter:=0
+		for{
+			select{
+			case data:=<- channel1:
+				fmt.Println("Data Dari channel 1 ",data)
+				counter++
+			case data:=<- channel2:
+				fmt.Println("Data Dari channel 2 ",data)
+				counter++
+			default:
+				fmt.Println("Menunggu Data")
+			}
+			if(counter == 2){
+				break
+			}
+		}
+	}
