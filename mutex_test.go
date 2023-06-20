1883f34673a5d77b423c1d2c933729e1f900b445
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-06-21 04:15:10 +0700

    RWMutex

--- mutex_test.go
+++ mutex_test.go
@@ -22,3 +22,37 @@ func TestMutex(t *testing.T) {
 	time.Sleep(5 * time.Second)
 	fmt.Println("Counter = ",x)
 }
+
+type BankAccount struct{
+	RWMutext sync.RWMutex
+	balance int
+}
+
+func (account *BankAccount) addBalance(amount int){
+	account.RWMutext.Lock()
+	account.balance = account.balance + amount
+	account.RWMutext.Unlock()
+}
+
+func (account *BankAccount) getBalance() int {
+	account.RWMutext.RLock()
+	balance:= account.balance
+	account.RWMutext.RUnlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account:= BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func(){
+			for j := 0; j < 100; j++ {
+				account.addBalance(1)
+				fmt.Println(account.getBalance())
+			}
+		}()
+	}
+	time.Sleep(5*time.Second)
+	fmt.Println("Total Account = ",account.getBalance())
+
+}
